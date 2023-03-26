82bcaa27d3e671f71501726bb3586ec649ca3494
sondt <sondt.vn@gmail.com>
2023-03-27 00:04:08 +0700

    Change port

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World! 👋!")
 	})
-	port := "80"
+	port := "3000"
 	if os.Getenv("ASPNETCORE_PORT") != "" { // get enviroment variable that set by ACNM
 		port = os.Getenv("ASPNETCORE_PORT")
 	}
